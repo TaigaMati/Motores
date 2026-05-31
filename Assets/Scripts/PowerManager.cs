8bcf0a84de440d6c01989fb065a8ddb7ad21088d
produccionvideojuegos <asesor.ia.tools@gmail.com>
2026-05-31 12:37:14 -0300

    Panel de texto de aviso sobre el generador sin combustible

--- Assets/Scripts/PowerManager.cs
+++ Assets/Scripts/PowerManager.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PowerManager : MonoBehaviour
 {
     public static PowerManager Instance;
 
     public bool powerOn = false;
+
+    // para activar panel de texto
+    [Header("Referencia al texto UI")]
+    public GameObject panelText;
+    private bool activePanelText = false;
 
     void Awake()
     {
@@ -20,5 +26,15 @@ public class PowerManager : MonoBehaviour
     {
         powerOn = false;
         Debug.Log(" Energía cortada");
+        // para activar panel de texto
+        activePanelText = true;
+        panelText.gameObject.SetActive(true);
+        StartCoroutine(HideTextAfterSeconds(15f));
+    }
+
+    private System.Collections.IEnumerator HideTextAfterSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        panelText.gameObject.SetActive(false);
     }
 }
